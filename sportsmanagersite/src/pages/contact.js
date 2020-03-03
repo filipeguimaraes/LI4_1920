4c90543c6d7f4dae5972d3d9a1ac9a519366c7f4
Beatriz Rocha <beatrizfr.rocha@yahoo.com>
2020-03-03 22:46:44 +0000

    register page almost done

--- sportsmanagersite/src/pages/contact.js
+++ sportsmanagersite/src/pages/contact.js
@@ -7,32 +7,27 @@ import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 const Contact = () => (
     <div className="contact-body">
         <Grid className="contact-grid">
-        <Cell col={6}>
-            
-            {/* <hr /> */}
-            <div className="contact-list">
-            <h2>Contact Us</h2>
-                <List>
-                    <ListItem>
-                        <ListItemContent style={{ fontSize: '30px', color: '#85D8CE' }}>
-                            <FontAwesomeIcon icon={faPhone} />
-                            &nbsp;
-                                123 456 789
+            <Cell col={6}>
+                <div className="contact-list">
+                    <h2>Contact Us</h2>
+                    <List>
+                        <ListItem>
+                            <ListItemContent style={{ fontSize: '30px', color: '#85D8CE' }}>
+                                <FontAwesomeIcon icon={faPhone} />
+                                &nbsp;
+                                    123 456 789
                             </ListItemContent>
-                    </ListItem>
-
-                    <ListItem>
-                        <ListItemContent style={{ fontSize: '30px', color: '#85D8CE' }}>
-                            <FontAwesomeIcon icon={faEnvelope} />
-                            &nbsp;
-                                someone@example.com
-</ListItemContent>
-                    </ListItem>
-
-                </List>
-            </div>
-        </Cell>
-        
+                        </ListItem>
+                        <ListItem>
+                            <ListItemContent style={{ fontSize: '30px', color: '#85D8CE' }}>
+                                <FontAwesomeIcon icon={faEnvelope} />
+                                &nbsp;
+                                 someone@example.com
+                            </ListItemContent>
+                        </ListItem>
+                    </List>
+                </div>
+            </Cell>
             <Cell col={6}>
                 <form className="login-form">
                     <input type="text" placeholder="email" required />
@@ -41,7 +36,6 @@ const Contact = () => (
                     <button>submit</button>
                 </form>
             </Cell>
-
         </Grid>
     </div>
 );
--- sportsmanagersite/src/pages/signup.js
+++ sportsmanagersite/src/pages/signup.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import '../styles/signup.css'
 
-function Signup() {
-  return (
-    <div class="login-page">
-      <div class="form">
-        <form class="login-form">
-          <input type="text" placeholder="email" />
-          <input type="password" placeholder="password" />
-          <input type="password" placeholder="confirm password" />
-          <button>Sign up</button>
-          <p class="message">Already registered? <a href="/login">Log In</a></p>
-        </form>
-      </div>
+
+const Signup = () => (
+  <div class="login-page">
+    <div class="form">
+      <form action="/register" class="login-form">
+        <input type="text" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <input type="password" placeholder="confirm password" />
+        <button>Sign up</button>
+        <p class="message">Already registered? <a href="/login">Log In</a></p>
+      </form>
     </div>
-  );
-}
+  </div>
+);
 
 export default Signup;
