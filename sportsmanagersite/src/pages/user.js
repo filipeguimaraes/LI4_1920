6dc15d77a6335c3086f14f9eabe240f2e5aa4a1c
Beatriz Rocha <beatrizfr.rocha@yahoo.com>
2020-03-07 22:02:46 +0000

    decline notifications missing

--- sportsmanagersite/src/pages/user.js
+++ sportsmanagersite/src/pages/user.js
@@ -9,6 +9,9 @@ import classIcon from '../images/menu-2.png';
 import statistcsIcon from '../images/menu-3.png';
 import settingsIcon from '../images/menu-4.png';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBasketballBall, faBiking} from '@fortawesome/free-solid-svg-icons'
+
 import Layout from '../layouts/UserLayout';
 
 const menus = [
@@ -38,6 +41,27 @@ const menus = [
     }
 ]
 
+const notes = [
+    {
+        "flag": "flag note",
+        "img": "flag__image note__icon",
+        "icon": faBasketballBall,
+        "text": "flag__body note__text",
+        "notification": "Book basketball court",
+        "ref": "note__close",
+        "cross": "fa fa-times"
+    },
+    {
+        "flag": "flag note note--secondary",
+        "img": "flag__image note__icon",
+        "icon": faBiking,
+        "text": "flag__body note__text",
+        "notification": "Attend cycling class",
+        "ref": "note__close",
+        "cross": "fa fa-times"
+    }
+]
+
 const User = () => (
     <Layout>
         <div style={{ width: '100%', margin: 'auto' }}>
@@ -52,7 +76,6 @@ const User = () => (
                         <h1 style={{ color: '#85D8CE', textAlign: "center" }}>Welcome, John!</h1>
                     </div>
                 </Cell>
-
                 <Cell col={12}>
                     <div id="icon-wrapper">
                         {menus.map(menu => (
@@ -66,81 +89,22 @@ const User = () => (
                         ))}
                     </div>
                 </Cell>
-
                 <Cell style={{ margin: '80px' }} col={12}>
                     <div class="sugestions-container">
                         <h1 style={{ color: '#85D8CE', textAlign: "left" }}>Based on your last purchases</h1>
-                        <div class="flag note">
-                            <div class="flag__image note__icon">
-                                <i class="fa fa-heart"></i>
+                        {notes.map(note => (
+                            <div class={note.flag}>
+                                <div class={note.img}>
+                                <FontAwesomeIcon icon={note.icon} />
+                                </div>
+                                <div class={note.text}>
+                                    {note.notification}
+                                </div>
+                                <a href="#" class={note.ref}>
+                                    <i class={note.cross}></i>
+                                </a>
                             </div>
-                            <div class="flag__body note__text">
-                                Default color
-    </div>
-                            <a href="#" class="note__close">
-                                <i class="fa fa-times"></i>
-                            </a>
-                        </div>
-                        <div class="flag note note--secondary">
-                            <div class="flag__image note__icon">
-                                <i class="fa fa-comment"></i>
-                            </div>
-                            <div class="flag__body note__text">
-                                Secondary color
-    </div>
-                            <a href="#" class="note__close">
-                                <i class="fa fa-times"></i>
-                            </a>
-                        </div>
-
-                        <div class="flag note note--success">
-                            <div class="flag__image note__icon">
-                                <i class="fa fa-check"></i>
-                            </div>
-                            <div class="flag__body note__text">
-                                Your thing was successful!
-    </div>
-                            <a href="#" class="note__close">
-                                <i class="fa fa-times"></i>
-                            </a>
-                        </div>
-
-                        <div class="flag note note--warning">
-                            <div class="flag__image note__icon">
-                                <i class="fa fa-exclamation"></i>
-                            </div>
-                            <div class="flag__body note__text">
-                                Your thing was not so successful!
-    </div>
-                            <a href="#" class="note__close">
-                                <i class="fa fa-times"></i>
-                            </a>
-                        </div>
-
-                        <div class="flag note note--error">
-                            <div class="flag__image note__icon">
-                                <i class="fa fa-times"></i>
-                            </div>
-                            <div class="flag__body note__text">
-                                Your thing failed completely!
-    </div>
-                            <a href="#" class="note__close">
-                                <i class="fa fa-times"></i>
-                            </a>
-                        </div>
-
-                        <div class="flag note note--info">
-                            <div class="flag__image note__icon">
-                                <i class="fa fa-info"></i>
-                            </div>
-                            <div class="flag__body note__text">
-                                <p>I barely knew Philip, but as a clergyman I have no problem telling his most intimate friends all about him. Calculon is gonna kill us and it's all everybody else's fault! Is that a cooking show? When will that be? Shut up and get to the point! Bender, being God isn't easy. If you do too much, people get dependent on you, and if you do nothing, they lose hope. You have to use a light touch. Like a safecracker, or a pickpocket.</p>
-                            </div>
-                            <a href="#" class="note__close">
-                                <i class="fa fa-times"></i>
-                            </a>
-                        </div>
-
+                        ))}
                     </div>
                 </Cell>
             </Grid>
