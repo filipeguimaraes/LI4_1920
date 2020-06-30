266f5d45b046310101c6bbb2fc568e3f85d1b0ba
Filipe Guimarães <fmtfg99@gmail.com>
2020-06-30 11:21:45 +0100

    tabelas classes e places

--- sportsmanagersite/src/pages/classes.js
+++ sportsmanagersite/src/pages/classes.js
@@ -3,6 +3,8 @@ import Layout from '../layouts/UserLayout';
 
 
 import MaterialTable from 'material-table';
+import CancelIcon from '@material-ui/icons/Cancel';
+import ControlPointIcon from '@material-ui/icons/ControlPoint';
 
 import '../styles/classes.css';
 
@@ -16,6 +18,15 @@ const values1 = [
     { id: 'Aerobics', title: 'Gym', priority: 2.90, type: '20', complete: '12:00', incomplete: '13:00' },
     { id: 'Cycling', title: 'Gym', priority: 1.25, type: '15', complete: '16:00', incomplete: '17:00' },
     { id: 'Tennis', title: 'Tennis court', priority: 2.35, type: '6', complete: '18:00', incomplete: '20:00' }
+];
+
+const showcolumns = [
+    { title: "Class", field: "id" },
+    { title: "Place", field: "title" },
+    { title: "Price", field: "priority", type: "currency" },
+    { title: "Capacity", field: "type", type: "numeric" },
+    { title: "Begin", field: "complete", type: "time" },
+    { title: "End", field: "incomplete", type: "time" }
 ];
 
 
@@ -43,51 +54,33 @@ class Classes extends Component {
                 <MaterialTable
                     actions={[
                         {
-                            icon: 'delete',
-                            tooltip: 'Cancel Class',
+                            icon: CancelIcon,
+                            tooltip: 'Enroll Class',
                             onClick: (event, rowData) => {
                                 // Do cancel class
                             }
                         }
                     ]}
-                    columns={[
-                        { title: "Class", field: "id" },
-                        { title: "Place", field: "title" },
-                        { title: "Price", field: "priority", type: "currency" },
-                        { title: "Capacity", field: "type", type: "numeric" },
-                        { title: "Begin", field: "complete", type: "time" },
-                        { title: "End", field: "incomplete", type: "time" }
-                    ]}
+                    columns={this.showcolumns}
                     data={this.state.data}
                     title=""
                 />
 
                 <h2 style={{ margin: '35px 5px 5px 75px', color: '#85D8CE' }}>Available classes</h2>
                 <MaterialTable
-                    columns={[
-                        { title: "Class", field: "id" },
-                        { title: "Place", field: "title" },
-                        { title: "Price", field: "priority", type: "currency" },
-                        { title: "Capacity", field: "type", type: "numeric" },
-                        { title: "Begin", field: "complete", type: "time" },
-                        { title: "End", field: "incomplete", type: "time" }
+                    actions={[
+                        {
+                            icon: ControlPointIcon,
+                            tooltip: 'Cancel Class',
+                            onClick: (event, rowData) => {
+                                // Do cancel class
+                            }
+                        }
                     ]}
+                    columns={this.showcolumns}
                     data={this.state.data}
                     title=""
-                    detailPanel={rowData => {
-                        return (
-                            <iframe
-                                width="100%"
-                                height="315"
-                                src="https://www.youtube.com/embed/C0DPdy98e4c"
-                                frameborder="0"
-                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                allowfullscreen
-                            />
-                        )
-                    }}
                 />
-
             </Layout>
         );
     }
--- sportsmanagersite/src/pages/places.js
+++ sportsmanagersite/src/pages/places.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import Layout from '../layouts/UserLayout';
 
+import MaterialTable from 'material-table';
+import CancelIcon from '@material-ui/icons/Cancel';
+import ControlPointIcon from '@material-ui/icons/ControlPoint';
+
 import '../styles/classes.css';
 
 import { checkAuthentication, validateAuth } from '../components/WebAPI';
 import { validUser } from './user';
 
-import TablePlaces from '../components/TablePlaces';
 
 
 const values1 = [
     { id: 'Gym', title: 'Braga', priority: 170, type: 20, complete: '20', incomplete: 'Sunny', begin: '16:00', end: '18:00' },
     { id: 'Tennis court', title: 'Guimarães', priority: 100, type: 15, complete: '10', incomplete: 'Rainy', begin: '14:00', end: '16:00' },
     { id: 'Basketball court', title: 'Viana do Castelo', priority: 120, type: 12, complete: '15', incomplete: 'Cloudy', begin: '10:00', end: '12:00' }
+];
+
+const showcolumns = [
+    { title: "Class", field: "id" },
+    { title: "Place", field: "title" },
+    { title: "Price", field: "priority", type: "currency" },
+    { title: "Capacity", field: "type", type: "numeric" },
+    { title: "Begin", field: "complete", type: "time" },
+    { title: "End", field: "incomplete", type: "time" }
 ];
 
 class Places extends Component {
@@ -35,10 +47,39 @@ class Places extends Component {
 
         return (
             <Layout>
-                <h2 style={{ margin: '35px 0px 0px 75px', color: '#85D8CE' }}>Next appointments</h2>
-                <TablePlaces data={this.state.data} toRemove={true} />
-                <h2 style={{ margin: '35px 0px 0px 75px', color: '#85D8CE' }}>Available places</h2>
-                <TablePlaces data={this.state.data} toRemove={false} />
+                <h2 style={{ margin: '35px 5px 5px 75px', color: '#85D8CE' }}>Next appointments</h2>
+                <MaterialTable
+                    actions={[
+                        {
+                            icon: CancelIcon,
+                            tooltip: 'Enroll Class',
+                            onClick: (event, rowData) => {
+                                // Do cancel class
+                            }
+                        }
+                    ]}
+                    columns={this.showcolumns}
+                    data={this.state.data}
+                    title=""
+                />
+                <h2 style={{ margin: '35px 5px 5px 75px', color: '#85D8CE' }}>Available places</h2>
+                <MaterialTable
+                    columns={this.showcolumns}
+                    data={this.state.data}
+                    title=""
+                    detailPanel={rowData => {
+                        return (
+                          <iframe
+                            width="100%"
+                            height="315"
+                            src="https://www.youtube.com/embed/C0DPdy98e4c"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen
+                          />
+                        )
+                      }}
+                />
             </Layout>
         );
     }
